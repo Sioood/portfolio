e1f706144b07149c40bea477dc9f91450576435a
Siod <74300009+Sioood@users.noreply.github.com>
2021-06-18 19:12:01 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -10,11 +10,38 @@ window.onload = () => {
 const logoRotation = document.querySelector("#logo");
 const translateCitation = document.querySelector(".citation-scroll-txt");
 
+/* window.onscroll = () => {
+  let posRotation = window.scrollY / 10;
+  logoRotation.style.transform = `rotate(${posRotation}deg)`;
+
+  let posCitation = window.scrollY * 2.5 - (window.innerWidth + 2250);
+  translateCitation.style.left = `${-posCitation}px`;
+}; */
+
 window.onscroll = () => {
   let posRotation = window.scrollY / 10;
   logoRotation.style.transform = `rotate(${posRotation}deg)`;
+
   let posCitation = window.scrollY * 2.5 - (window.innerWidth + 2250);
   translateCitation.style.left = `${-posCitation}px`;
 };
+
+const navLink = document.querySelectorAll(".navbar-menu-link")
+
+navLink .forEach(link => {
+    link.addEventListener('click', (e) => {
+
+        // console.log(e.target);
+        e.target.classList.add('active');
+
+        for(let i = 0; i < navLink.length; i++ ){
+            if(navLink[i] !== e.target){
+              navLink[i].classList.remove('active');
+            };
+        };
+        
+
+    });
+});
 
 // elem.style.transform = `translatex(-50%) translatey(-50%) rotate(${value}deg)`;
