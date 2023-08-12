7d22740ed04630dc4f84f5cda99ab1d97f16f658
Sioood <theodupontpro@gmail.com>
2023-08-12 18:37:55 +0200

    📱 style: update responsive

--- pages/index.vue
+++ pages/index.vue
@@ -97,12 +97,18 @@ definePageMeta({
     <section
       class="relative min-h-screen flex flex-col items-center overflow-x-clip"
     >
-      <div class="z-[1] absolute bottom-[7rem] flex flex-col items-end">
-        <h1 class="text-[40rem] leading-[10rem] italic text-primary-200">
+      <div
+        class="z-[1] absolute bottom-[1rem] md:bottom-[7rem] flex flex-col items-end"
+      >
+        <h1
+          class="text-[10rem] md:text-[40rem] leading-[4rem] md:leading-[10rem] italic text-primary-200"
+        >
           &nbsp; Théo <br />
           Dupont
         </h1>
-        <h6 class="-translate-x-4 absolute top-0 left-2/3">
+        <h6
+          class="-translate-x-4 absolute top-2/3 md:top-0 left-1/3 md:left-2/3"
+        >
           [Développeur Front End] (Graphiste)
         </h6>
       </div>
@@ -122,7 +128,7 @@ definePageMeta({
       </div>
 
       <div
-        class="position sticky px-52 w-full min-h-screen flex items-center justify-end pointer-events-none"
+        class="position sticky px-10 md:px-52 w-full min-h-screen flex items-center justify-end pointer-events-none"
       >
         <h2 class="text-8xl italic text-background">Curiosité</h2>
       </div>
@@ -137,11 +143,13 @@ definePageMeta({
         data-animate-element="contact"
         class="sticky top-0 w-full h-screen pb-16 flex flex-col justify-between bg-background"
       >
-        <div class="flex items-center">
-          <h2 class="text-5xl px-40 w-1/2 italic">
-            Je suis disponible pour vos projets. Prenons contact !
+        <div
+          class="mt-10 md:mt-0 p-4 md:p-0 flex flex-col md:flex-row items-center gap-16 md:gap-0"
+        >
+          <h2 class="text-3xl md:text-5xl md:px-40 md:w-1/2 italic">
+            Je suis disponible pour vos idées. Prenons contact !
           </h2>
-          <div class="w-1/2 flex items-end justify-center gap-3">
+          <div class="md:w-1/2 flex items-end md:justify-center gap-3">
             <div data-flip-id="1" class="w-1/3 aspect-[9/13] bg-surface">
               <img
                 src="https://images.unsplash.com/photo-1691229732670-f7034e54d2cb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"
@@ -155,10 +163,10 @@ definePageMeta({
             </h6>
           </div>
         </div>
-        <div class="flex justify-center">
-          <div class="flex flex-col items-end">
-            <h2 class="text-8xl italic">Théo Dupont</h2>
-            <p class="w-[20rem]">
+        <div class="px-4 flex justify-center">
+          <div class="flex flex-col md:items-end">
+            <h2 class="text-5xl md:text-8xl italic">Théo Dupont</h2>
+            <p class="md:w-[20rem]">
               Hi there! I’m a freelance designer & developer from Canada with a
               love for all things web. I’m currently working with clients &
               collaborating with agencies worldwide.
@@ -171,15 +179,16 @@ definePageMeta({
         data-animate-element="contact"
         class="sticky top-0 w-full h-screen pb-16 flex flex-col justify-between bg-background"
       >
-        <div class="flex items-center">
-          <h2 class="text-5xl px-40 w-1/2 italic">
+        <div
+          class="mt-10 md:mt-0 p-4 md:p-0 flex flex-col md:flex-row items-center gap-16 md:gap-0"
+        >
+          <h2 class="text-3xl md:text-5xl md:px-40 md:w-1/2 italic">
             Je suis disponible pour vos idées. Prenons contact !
           </h2>
-          <div class="w-1/2 flex items-end justify-center gap-3">
-            <div class="w-1/3 aspect-[9/13] bg-surface">
+          <div class="md:w-1/2 flex items-end md:justify-center gap-3">
+            <div data-flip-id="1" class="w-1/3 aspect-[9/13] bg-surface">
               <img
-                ref="flipElement"
-                src="https://images.unsplash.com/photo-1691293881543-9b891bd9b6b5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"
+                src="https://images.unsplash.com/photo-1513171920216-2640b288471b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=388&q=80"
                 alt=""
                 class="w-full h-full object-cover"
               />
@@ -190,10 +199,10 @@ definePageMeta({
             </h6>
           </div>
         </div>
-        <div class="flex justify-center">
-          <div class="flex flex-col items-end">
-            <h2 class="text-8xl italic">Théo Dupont</h2>
-            <p class="w-[20rem]">
+        <div class="px-4 flex justify-center">
+          <div class="flex flex-col md:items-end">
+            <h2 class="text-5xl md:text-8xl italic">Théo Dupont</h2>
+            <p class="md:w-[20rem]">
               Hi there! I’m a freelance designer & developer from Canada with a
               love for all things web. I’m currently working with clients &
               collaborating with agencies worldwide.
@@ -204,19 +213,25 @@ definePageMeta({
 
       <div
         data-animate-element="contact"
-        class="sticky top-0 w-full h-screen pb-16 flex flex-col justify-between bg-neutral-100 text-primary-200"
+        class="sticky top-0 w-full h-screen pb-16 flex flex-col justify-between bg-background text-primary-200"
       >
         <div
           class="-z-[1] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-1/2 aspect-square bg-neutral-900 rounded-full blur-3xl"
         ></div>
 
-        <div class="flex items-center">
-          <h2 class="text-5xl px-40 w-1/2 italic">
-            Je suis disponible pour vos envies. Prenons contact !
+        <div
+          class="mt-10 md:mt-0 p-4 md:p-0 flex flex-col md:flex-row items-center gap-16 md:gap-0"
+        >
+          <h2 class="text-3xl md:text-5xl md:px-40 md:w-1/2 italic">
+            Je suis impatient de travailler avec vous.
           </h2>
-          <div class="w-1/2 flex items-end justify-center gap-3">
-            <div class="w-1/3 aspect-[9/13] bg-surface">
-              <!-- <img src="" alt="" class="w-full h-full object-cover"> -->
+          <div class="md:w-1/2 flex items-end md:justify-center gap-3">
+            <div data-flip-id="1" class="w-1/3 aspect-[9/13] bg-surface">
+              <img
+                src="https://images.unsplash.com/photo-1558862108-daa1be6fda90?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"
+                alt=""
+                class="w-full h-full object-cover"
+              />
             </div>
             <h6 class="mb-10">
               theodupontpro@gmail.com <br />
@@ -224,10 +239,10 @@ definePageMeta({
             </h6>
           </div>
         </div>
-        <div class="flex justify-center">
-          <div class="flex flex-col items-end">
-            <h2 class="text-8xl italic">Théo Dupont</h2>
-            <p class="w-[20rem]">
+        <div class="px-4 flex justify-center">
+          <div class="flex flex-col md:items-end">
+            <h2 class="text-5xl md:text-8xl italic">Théo Dupont</h2>
+            <p class="md:w-[20rem]">
               Hi there! I’m a freelance designer & developer from Canada with a
               love for all things web. I’m currently working with clients &
               collaborating with agencies worldwide.
