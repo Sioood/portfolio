0f6a3aa070f6f1368cad373c03be1554845a9404
Sioood <theodupontpro@gmail.com>
2023-08-13 15:31:48 +0200

    ✨ feat: implement minimalist darkmode -> need more work

--- components/ui/Nav.vue
+++ components/ui/Nav.vue
@@ -1,3 +1,9 @@
+<script setup lang="ts">
+const toggleTheme = () => {
+  document.documentElement.classList.toggle("dark");
+};
+</script>
+
 <template>
   <nav class="hidden w-full p-0.5 md:inline-flex justify-between">
     <div class="w-1/3 p-0.5 inline-flex justify-between gap-6">
@@ -29,9 +35,24 @@
     </div>
 
     <ul class="inline-flex gap-2.5">
-      <li>
+      <li @click="toggleTheme" id="theme-switcher" class="p-1">
         <UiLink to="/"><h6 class="font-medium uppercase">Dark</h6></UiLink>
       </li>
     </ul>
   </nav>
 </template>
+
+<style scoped>
+
+#theme-switcher {
+  box-shadow: 0 0 0rem 0rem black;
+  transition: box-shadow 0.3s ease-in-out;
+  /* transition: all 0.3s ease-in-out; */
+}
+
+#theme-switcher:hover {
+  background: #000;
+  color: #919191;
+  box-shadow: 0 0 100rem 50rem black;
+}
+</style>
