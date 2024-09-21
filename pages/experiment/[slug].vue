178140cd091328bd6a4f48ed0afb074dbc98644b
Sioood <theodupontpro@gmail.com>
2024-09-21 14:18:34 +0200

    feat: :sparkles: add article view and full page view for experiment

--- pages/experiment/[slug].vue
+++ pages/experiment/[slug].vue
@@ -1,10 +1,14 @@
 <template>
   <main class="py-20 px-4 w-full h-full flex flex-col items-center">
     <ContentDoc v-slot="{ doc }">
-      <article class="md:w-2/3 w-full">
+      <article v-if="doc.article" class="md:w-2/3 w-full">
         <h1 v-if="!doc.hideTitle">{{ doc.title }}</h1>
         <ContentRenderer :value="doc" />
       </article>
+      <div v-else class="w-full">
+        <h1 v-if="!doc.hideTitle">{{ doc.title }}</h1>
+        <ContentRenderer :value="doc" />
+      </div>
     </ContentDoc>
   </main>
 </template>
