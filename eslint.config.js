af61ad843e894d15369183a5f297bb4bbac12b86
Sioood <theodupontpro@gmail.com>
2025-07-27 12:39:11 +0200

    build(config): :wrench: add husky, lint-staged and commitlint

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from "eslint/config"
-import base from "@devoid/eslint"
+import { defineConfig } from 'eslint/config'
+import base from '@devoid/eslint'
 
 export default defineConfig(base)
