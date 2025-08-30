Sioood/stallning

feat(global): :sparkles: rework eslint config

apps/web/eslint.config.js
@@ -1,4 +1,14 @@
-import { createConfigForNuxt } from '@nuxt/eslint-config'
-import baseVue from '@stallning/eslint/configs/vue'
+import eslint from '@stallning/eslint'
 
-export default createConfigForNuxt().append(baseVue)
+import { withNuxt } from './.nuxt/eslint.config.mjs'
+
+// TODO add tailwind
+export default withNuxt(
+  eslint({
+    oxlint: 'node_modules/@stallning/oxlint/.oxlintrc.json',
+    prettier: true,
+    typescript: true,
+    vue: true,
+    tailwindcss: 'node_modules/@stallning/ui/assets/css/main.css',
+  }),
+)

apps/web/i18n/locales/fr-FR/index.ts
@@ -1,4 +1,4 @@
-import { getMessagesWithNamespace } from '~nuxt-essentials/i18n/utils/namespace'
+import { getMessagesWithNamespace } from '@stallning/nuxt-essentials/i18n/utils/namespace'
 
 export default defineI18nLocale(async () => {
   const namespacesFiles = {

packages/config/eslint/src/configs/prettier.ts
@@ -1,0 +1,13 @@
+import prettierConfig from '@stallning/prettier'
+import pluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+
+import { defineConfig } from '../types.ts'
+
+export const prettier = defineConfig([
+  pluginPrettierRecommended,
+  {
+    rules: {
+      'prettier/prettier': ['error', prettierConfig],
+    },
+  },
+])

packages/nuxt-essentials/eslint.config.js
@@ -1,4 +1,12 @@
-import { createConfigForNuxt } from '@nuxt/eslint-config'
-import baseVue from '@stallning/eslint/configs/vue'
+import eslint from '@stallning/eslint'
 
-export default createConfigForNuxt().append(baseVue)
+import { withNuxt } from './.playground/.nuxt/eslint.config.mjs'
+
+export default withNuxt(
+  eslint({
+    oxlint: true,
+    prettier: true,
+    typescript: true,
+    vue: true,
+  }),
+)

packages/ui/eslint.config.js
@@ -1,5 +1,13 @@
-import { createConfigForNuxt } from '@nuxt/eslint-config'
-import baseVue from '@stallning/eslint/configs/vue'
-import baseTailwind from '@stallning/eslint/configs/tailwind'
+import eslint from '@stallning/eslint'
 
-export default createConfigForNuxt().append(baseTailwind, baseVue)
+import { withNuxt } from './.nuxt/eslint.config.mjs'
+
+export default withNuxt(
+  eslint({
+    oxlint: true,
+    prettier: true,
+    typescript: true,
+    tailwindcss: './assets/css/main.css',
+    vue: true,
+  }),
+)

Sioood/portfolio

apps/web/eslint.config.js
@@ -1,4 +1,14 @@
-import { createConfigForNuxt } from '@nuxt/eslint-config'
-import baseVue from '@portfolio/eslint/configs/vue'
+import eslint from '@portfolio/eslint'
 
-export default createConfigForNuxt().append(baseVue)
+import { withNuxt } from './.nuxt/eslint.config.mjs'
+
+// TODO add tailwind
+export default withNuxt(
+  eslint({
+    oxlint: 'node_modules/@portfolio/oxlint/.oxlintrc.json',
+    prettier: true,
+    typescript: true,
+    vue: true,
+    tailwindcss: 'node_modules/@portfolio/ui/assets/css/main.css',
+  }),
+)

apps/web/i18n/locales/fr-FR/index.ts
@@ -1,4 +1,4 @@
-import { getMessagesWithNamespace } from '~nuxt-essentials/i18n/utils/namespace'
+import { getMessagesWithNamespace } from '@portfolio/nuxt-essentials/i18n/utils/namespace'
 
 export default defineI18nLocale(async () => {
   const namespacesFiles = {

packages/config/eslint/src/configs/prettier.ts
@@ -1,0 +1,13 @@
+import prettierConfig from '@portfolio/prettier'
+import pluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+
+import { defineConfig } from '../types.ts'
+
+export const prettier = defineConfig([
+  pluginPrettierRecommended,
+  {
+    rules: {
+      'prettier/prettier': ['error', prettierConfig],
+    },
+  },
+])

packages/nuxt-essentials/eslint.config.js
@@ -1,4 +1,12 @@
-import { createConfigForNuxt } from '@nuxt/eslint-config'
-import baseVue from '@portfolio/eslint/configs/vue'
+import eslint from '@portfolio/eslint'
 
-export default createConfigForNuxt().append(baseVue)
+import { withNuxt } from './.playground/.nuxt/eslint.config.mjs'
+
+export default withNuxt(
+  eslint({
+    oxlint: true,
+    prettier: true,
+    typescript: true,
+    vue: true,
+  }),
+)

packages/ui/eslint.config.js
@@ -1,5 +1,13 @@
-import { createConfigForNuxt } from '@nuxt/eslint-config'
-import baseVue from '@portfolio/eslint/configs/vue'
-import baseTailwind from '@portfolio/eslint/configs/tailwind'
+import eslint from '@portfolio/eslint'
 
-export default createConfigForNuxt().append(baseTailwind, baseVue)
+import { withNuxt } from './.nuxt/eslint.config.mjs'
+
+export default withNuxt(
+  eslint({
+    oxlint: true,
+    prettier: true,
+    typescript: true,
+    tailwindcss: './assets/css/main.css',
+    vue: true,
+  }),
+)