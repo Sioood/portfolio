84ab497c46a6120985911b184cb5643bcaca3e8f
Sioood <theodupontpro@gmail.com>
2025-09-02 22:50:39 +0200

    fix(web): :lipstick: select none on title

--- apps/web/app/pages/index.vue
+++ apps/web/app/pages/index.vue
@@ -6,7 +6,7 @@ const { t } = useI18n()
   <div>
     <section class="relative flex min-h-screen flex-col items-center overflow-x-clip">
       <div class="absolute bottom-[5vw] z-[1] flex flex-col items-end xl:bottom-[3vw]">
-        <h1 class="font-secondary text-primary-200 text-[clamp(10rem,30vw,40rem)] leading-[7vw] font-light italic">
+        <h1 class="font-secondary text-primary-200 text-[clamp(10rem,30vw,40rem)] leading-[7vw] font-light italic select-none">
           &nbsp; {{ t('firstname') }} <br />
           {{ t('lastname') }}
         </h1>
