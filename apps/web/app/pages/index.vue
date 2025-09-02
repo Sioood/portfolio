126755526fb9a75777de77099ff17bfdf18d6c10
Théo Dupont <74300009+Sioood@users.noreply.github.com>
2025-09-02 23:14:16 +0200

    minimal preview (#1)
    * docs(global): :memo: update README
    
    * fix(web): :lipstick: select none on title
    
    * fix(config): :bug: lint-staged monorepo config
    
    * fix(config): :bug: format mode
    
    * feat(ui): :lipstick: add selection colors
    
    * feat(web): :dizzy: add text animation
    
    * feat(web): :mag: add favicon and title

--- apps/web/app/pages/index.vue
+++ apps/web/app/pages/index.vue
@@ -6,11 +6,11 @@ const { t } = useI18n()
   <div>
     <section class="relative flex min-h-screen flex-col items-center overflow-x-clip">
       <div class="absolute bottom-[5vw] z-[1] flex flex-col items-end xl:bottom-[3vw]">
-        <h1 class="font-secondary text-primary-200 text-[clamp(10rem,30vw,40rem)] leading-[7vw] font-light italic">
+        <h1 class="font-secondary text-primary-200 text-[clamp(10rem,30vw,40rem)] leading-[7vw] font-light italic select-none">
           &nbsp; {{ t('firstname') }} <br />
           {{ t('lastname') }}
         </h1>
-        <h6 class="absolute top-2/3 left-1/3 -translate-x-4 text-neutral-900 xl:top-0 xl:left-2/3">
+        <h6 v-gsap.animateText class="absolute top-2/3 left-1/3 -translate-x-4 text-neutral-900 xl:top-0 xl:left-2/3">
           {{
             `[${t('fullStackDeveloper')}]
           (${t('graphicDesigner')})`
