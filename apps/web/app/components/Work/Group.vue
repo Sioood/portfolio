2ad06ec3e435592793a0003e549cde09fab8c2d9
Sioood <theodupontpro@gmail.com>
2025-09-16 22:55:45 +0200

    refactor(web): :construction: works preview

--- apps/web/app/components/Work/Group.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="flex aspect-8/16 min-h-screen flex-col border border-red-500">
-    <div class="size-full border"><WorkItem /></div>
-    <div class="flex size-full justify-end border"><WorkItem /></div>
-  </div>
-</template>
