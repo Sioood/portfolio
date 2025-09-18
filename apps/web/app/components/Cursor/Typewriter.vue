4faa87e9555a1bc0ce4cee2ade6ca18e175720e2
Sioood <theodupontpro@gmail.com>
2025-09-18 22:30:40 +0200

    fix(web): :bug: prevent custom typewriter cursor on smaller device

--- apps/web/app/components/Cursor/Typewriter.vue
+++ apps/web/app/components/Cursor/Typewriter.vue
@@ -1,15 +1,19 @@
 <script setup lang="ts">
-import { useMouse } from '@vueuse/core'
+import { useMouse, breakpointsTailwind, useBreakpoints } from '@vueuse/core'
 const cursorRef = ref<HTMLDivElement | null>(null)
 
 const { x, y } = useMouse()
 
 const { text } = extractStore(useCursorStore())
+
+const breakpoints = useBreakpoints(breakpointsTailwind)
+const lgAndLarger = breakpoints.greaterOrEqual('lg')
 </script>
 
 <template>
   <Teleport to="#teleports">
     <div
+      v-if="lgAndLarger"
       ref="cursorRef"
       v-gsap.from="{ autoAlpha: 0 }"
       class="pointer-events-none absolute z-[999] flex -translate-y-1/2 items-center justify-center text-neutral-100 mix-blend-difference"
