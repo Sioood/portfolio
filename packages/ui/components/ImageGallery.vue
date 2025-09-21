04c7f2a05f8bfb56b966545b56177490aea6003d
Sioood <theodupontpro@gmail.com>
2025-09-21 20:03:40 +0200

    feat(web): :sparkles: add more gap between row

--- packages/ui/components/ImageGallery.vue
+++ packages/ui/components/ImageGallery.vue
@@ -4,7 +4,7 @@ const props = defineProps<{ images: { src: string; alt: string; caption?: string
 
 <template>
   <ul class="grid grid-cols-[repeat(auto-fill,minmax(max(250px,17.5vw),1fr))] gap-[5vw]">
-    <li v-for="image in props.images" :key="image.src" class="flex size-full flex-col gap-1">
+    <li v-for="image in props.images" :key="image.src" class="mt-[5vw] flex size-full flex-col gap-1">
       <NuxtImg :src="image.src" :alt="image.alt" class="w-full object-cover" format="webp" />
       <span v-if="image.caption" class="w-full text-right text-sm text-neutral-500">{{ image.caption }}</span>
     </li>
