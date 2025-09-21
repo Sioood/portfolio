9b900609d9a25b9801753cd262f92d2e9d70da2d
Sioood <theodupontpro@gmail.com>
2025-09-21 14:01:27 +0200

    feat(web): :sparkles: more appealing experiments

--- apps/web/app/components/Experiments/Card.vue
+++ apps/web/app/components/Experiments/Card.vue
@@ -4,10 +4,14 @@ const props = defineProps<Experiment>()
 </script>
 
 <template>
-  <div class="relative flex size-full items-center justify-center bg-neutral-900">
-    <span class="font-secondary absolute bottom-0 z-10 text-[5rem] font-light text-neutral-100 italic mix-blend-difference">{{
-      String(props.id).padStart(3, '0')
-    }}</span>
-    <NuxtImg v-if="props.preview" :src="props.preview" class="size-full object-cover" format="webp" />
+  <div class="flex size-full flex-col gap-3">
+    <div class="relative flex size-full items-center justify-center bg-neutral-900">
+      <NuxtImg v-if="props.preview" :src="props.preview" class="size-full object-cover" format="webp" />
+    </div>
+    <span class="truncate text-xs">
+      {{ String(props.id).padStart(3, '0') }}
+
+      <span v-if="props.caption"> — {{ props.caption }}</span>
+    </span>
   </div>
 </template>
--- apps/web/app/pages/Experiments/index.vue
+++ apps/web/app/pages/Experiments/index.vue
@@ -16,13 +16,13 @@ definePageMeta({
 
 <template>
   <div class="flex flex-col">
-    <ul v-if="experiments?.length" class="mt-10 grid grid-cols-[repeat(auto-fill,minmax(max(250px,17.5vw),1fr))] gap-4 p-4">
+    <ul v-if="experiments?.length" class="mt-10 grid grid-cols-[repeat(auto-fill,minmax(max(250px,17.5vw),1fr))] gap-[5vw] p-[5vw]">
       <UILink
         v-for="experiment in [...experiments].reverse()"
         v-slot="{ isLinkButton }"
         :key="experiment.id"
         :to="`/experiments/${experiment.id}/${experiment.slug}`"
-        class="size-full [&>*>*>*>img]:filter-[invert(100%)_grayscale(100%)] [&>*>*>*>img]:!transition-[filter] [&>*>*>*>img]:duration-1000 [&>*>*>*>img]:ease-out hover:[&>*>*>*>img]:filter-[invert(0%)_grayscale(0%)] hover:[&>*>*>*>img]:!transition-none"
+        class="size-full [&_img]:filter-[invert(100%)_grayscale(100%)] [&_img]:!transition-[filter] [&_img]:duration-1000 [&_img]:ease-out hover:[&_img]:filter-[invert(0%)_grayscale(0%)] hover:[&_img]:!transition-none"
       >
         <UIButton
           v-gsap.stagger.fromTo="[
