f9ff118704f864e282fef68ab2f52f1c558a7a6f
Sioood <theodupontpro@gmail.com>
2025-09-22 18:03:22 +0200

    feat(web): :sparkles: rework work card and section

--- apps/web/app/components/Work/Card.vue
+++ apps/web/app/components/Work/Card.vue
@@ -1,94 +1,37 @@
 <script setup lang="ts">
-import { cva, type VariantProps } from 'class-variance-authority'
-
-const imageList = cva('imageList', {
-  variants: {
-    overflow: {
-      hidden: 'overflow-hidden',
-      visible: 'overflow-visible',
-    },
-    gap: {
-      sm: 'gap-4',
-      md: 'gap-8',
-    },
-  },
-})
-
-type ImageListProps = VariantProps<typeof imageList>
-
-const image = cva('image', {
-  variants: {
-    rounded: {
-      't-full': 'rounded-t-full',
-      full: 'rounded-full',
-    },
-    aspect: {
-      default: 'size-full',
-      '16/9': 'w-full aspect-16/9',
-      '9/16': 'h-full aspect-9/16',
-    },
-  },
-})
-
-type ImageProps = VariantProps<typeof image>
-
-interface Work {
-  slug: string
-  title: string
-  year: number
-  images: { src: string; alt?: string }[]
-}
-const props = withDefaults(
-  defineProps<{
-    data: Work
-    ui?: {
-      imageList?: ImageListProps
-      image?: ImageProps
-    }
-  }>(),
-  {
-    ui: () => ({
-      imageList: {
-        overflow: 'hidden',
-      },
-      image: {
-        aspect: 'default',
-      },
-    }),
-  },
-)
+const props = defineProps<{
+  work: Work
+}>()
 
 const { visible, text: cursorText } = extractStore(useCursorStore())
+
+const year = computed(() => Number(props.work.date.split('-')[0] || props.work.date))
 </script>
 
 <template>
-  <div class="relative flex h-screen w-full flex-col items-center justify-center gap-8 overflow-hidden md:gap-[0]">
+  <div class="relative flex w-full flex-col items-center justify-center overflow-hidden py-[15dvh]">
     <UILink
-      :to="`/work/${props.data.slug}`"
-      class="flex h-1/2 w-3/5 items-center justify-center active:scale-97"
-      :class="imageList(props.ui.imageList)"
-      @mouseenter="((visible = true), (cursorText = `${props.data.title}, ${props.data.year}`))"
+      :to="`/work/${props.work.slug}`"
+      class="flex w-[90%] flex-col items-center justify-center gap-1 active:scale-97 lg:w-2/5 2xl:w-2/7"
+      @mouseenter="((visible = true), (cursorText = `${props.work.title}, ${year}`))"
       @mouseleave="((visible = false), (cursorText = ''))"
       @click="((visible = false), (cursorText = ''))"
     >
-      <div v-for="img in props.data.images" :key="img.src" class="flex items-end overflow-hidden" :class="image(props.ui.image)">
-        <NuxtImg
-          v-gsap.parallax.slower-2
-          :src="img.src"
-          :alt="img?.alt || `${props.data.title}, ${props.data.year}`"
-          class="size-[110%] object-cover"
-          format="webp"
-        />
-      </div>
-    </UILink>
+      <span class="w-full text-right">{{ String(work.id).padStart(3, '0') }}</span>
 
-    <UILink
-      v-gsap.parallax.slower-10
-      :to="`/work/${props.data.slug}`"
-      class="inline-flex w-full items-center justify-between px-8 text-lg text-neutral-100 mix-blend-difference active:scale-97 md:absolute"
-    >
-      <span>{{ props.data.title }}</span>
-      <span data-split-text>{{ props.data.year }}</span>
+      <NuxtImg
+        :src="work.preview?.src || work.leftImage?.src"
+        :alt="work.preview?.alt || `${props.work.title}, ${Number(work.date.split('-')[0] || work.date)}`"
+        class="size-full object-cover"
+        format="webp"
+      />
+
+      <div class="inline-flex w-full justify-between gap-2">
+        <span class="truncate">{{ work.title }}</span>
+        <span>
+          {{ year }}
+        </span>
+      </div>
     </UILink>
   </div>
 </template>
